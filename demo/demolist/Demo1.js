81ae3406a3b326cdce37b65b54f47bcd12729c61
ahua52 <1468492018@qq.com>
2017-08-21 16:34:07 +0800

    city-select

---
+++ demo/demolist/Demo1.js
@@ -0,0 +1,13 @@
+/**
+*
+* @title 地区级联
+* @description 中国地区级联
+*
+*/
+class Demo1 extends Component {
+	render () {
+		return (
+			<CitySelect />
+		)
+	}
+}
---
+++ demo/index.js
@@ -0,0 +1,84 @@
+
+import { Con, Row, Col } from 'bee-layout';
+import { Panel } from 'bee-panel';
+import Button from 'bee-button';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import CitySelect from '../src';
+
+
+const CARET = <i className="uf uf-arrow-down"></i>;
+
+const CARETUP = <i className="uf uf-arrow-up"></i>;
+
+
+/**
+*
+* @title 地区级联
+* @description 中国地区级联
+*
+*/
+class Demo1 extends Component {
+	render () {
+		return (
+			<CitySelect />
+		)
+	}
+}var DemoArray = [{"example":<Demo1 />,"title":" 地区级联","code":"/**\n*\n* @title 地区级联\n* @description 中国地区级联\n*\n*/\nclass Demo1 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<CitySelect />\n\t\t)\n\t}\n}","desc":" 中国地区级联"}]
+
+
+class Demo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({ open: !this.state.open })
+    }
+
+    render () {
+        const { title, example, code, desc  } = this.props;
+        let caret = this.state.open ? CARETUP : CARET;
+        let text = this.state.open ? "隐藏代码" : "查看代码";
+
+        const footer = (
+            <Button shape="block" onClick={ this.handleClick }>
+                { caret }
+                { text }
+            </Button>
+        );
+        return (
+            <Col md={12} >
+                <h3>{ title }</h3>
+                <p>{ desc }</p>
+                <Panel collapsible headerContent expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0}}>
+                    <pre><code className="hljs javascript">{ code }</code></pre>
+                </Panel>
+            </Col>
+        )
+    }
+}
+
+class DemoGroup extends Component {
+    constructor(props){
+        super(props)
+    }
+    render () {
+        return (
+                <Row>
+                    {DemoArray.map((child,index) => {
+
+                        return (
+                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
+                        )
+
+                    })}
+                </Row>
+        )
+    }
+}
+
+ReactDOM.render(<DemoGroup/>, document.getElementById('tinperBeeDemo'));
